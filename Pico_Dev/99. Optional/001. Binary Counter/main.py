a76b47661cf065e9569b92439da97a654881b57c
Ettiene-PC <ettienemare@gmail.com>
2025-02-22 15:34:28 +0200

    Add Binary Counter

--- Pico_Dev/99. Optional/001. Binary Counter/main.py
+++ Pico_Dev/99. Optional/001. Binary Counter/main.py
@@ -1,17 +1,18 @@
 import machine
 import utime
 
-pin1 = machine.Pin(15, machine.Pin.OUT)
-pin2 = machine.Pin(15, machine.Pin.OUT)
-pin3 = machine.Pin(15, machine.Pin.OUT)
-pin4 = machine.Pin(15, machine.Pin.OUT)
+pin1 = machine.Pin('LED', machine.Pin.OUT)
+pin2 = machine.Pin(12, machine.Pin.OUT)
+pin3 = machine.Pin(13, machine.Pin.OUT)
+pin4 = machine.Pin(14, machine.Pin.OUT)
 pin5 = machine.Pin(15, machine.Pin.OUT)
 
 pins = [pin1, pin2, pin3, pin4, pin5]
 
 
 for i in range(0, 2**len(pins)):
-    binary_string = format(i, f'0{len(pins)}b')
+    binary_string = '{:0{}b}'.format(i, len(pins))
+    print(binary_string)
 
     for j in range(len(pins)):
         pins[j].value(int(binary_string[j]))
